76856f0acc4cbeb250f268900a192fef4d56e389
Mesach20002 <adrianmesach28@gmail.com>
2026-07-11 17:40:32 +0300

    Fix next.config and run locally

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // ... other config
-}
+};
 
-export default nextConfig
+export default nextConfig;
